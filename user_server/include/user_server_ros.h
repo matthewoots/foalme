38630bb58cf24a2b943dfb38408375881f064ac9
matthewoots <matthewoots@gmail.com>
2022-06-14 18:03:38 +0800

    [librrtserver] Change made to solve the poor Z axis search, and to clamp to the min and max height [bs-traj-server] Fixed some bugs regarding bspline taking one less control points (for concatenation) which increases the speed of the UAV. There are other bug fixes and improvements to the performance [simple_quad_simulator] Added subscriber for global map, and message that can be received by the user_server [user_server] Added dissemination of message (uav info) and waypoints also including functionality for multi agents

--- user_server/include/user_server_ros.h
+++ user_server/include/user_server_ros.h
@@ -52,6 +52,13 @@ class user_server_ros
             Eigen::Vector3d pos;
             Eigen::Quaterniond q;
             ros::Time t;
+            int mission;
+        };
+
+        struct agent_waypoint
+        {
+            vector<Eigen::Vector3d> waypoints;
+            int id;
         };
 
         ros::NodeHandle _nh;
@@ -62,13 +69,13 @@ class user_server_ros
 
         ros::Timer _target_tracker_timer, _cloud_timer;
 
-        geometry_msgs::Point goal_pose;
-
-        std::string _file_location;
-
-        Eigen::Vector3d goal;
+        std::string _file_location, _single_or_multi, _formation;
 
         pcl::PointCloud<pcl::PointXYZ>::Ptr global_cloud;  
+
+        bool _valid_cloud;
+
+        int _agent_number;
 
         double _tracker_timer_hz, _cloud_hz; 
 
@@ -76,17 +83,96 @@ class user_server_ros
 
         sensor_msgs::PointCloud2 cloud_msg;
 
+        vector<agent_waypoint> agent_waypoints;
+        // vector<Eigen::Vector3d> waypoints;
+
+        ros::Time module_start_time;
+
         void target_update_timer(const ros::TimerEvent &)
         {
-            std::string _id;
-            /** @brief Publisher that publishes goal vector */
-            _goal_pub = _nh.advertise<geometry_msgs::PoseStamped>("/" + _id + "/goal", 20);
+            if ((ros::Time::now() - module_start_time).toSec() < 3.0)
+            {
+                return;
+            }
             
+            if (agent_waypoints[0].waypoints.empty() && !agents.empty() && _agent_number > 1)
+            {
+                for (int i = 0; i < agents.size(); i++)
+                {
+
+                }
+            }
+
+            if (agents.empty())
+                return;
+            
+            for (int i = 0; i < agents.size(); i++)
+            {
+                std::string _id;
+                _id = "drone" + to_string(agents[i].id);
+
+                if (agents[i].mission >= 0)
+                {
+                    if ((agent_waypoints[agents[i].id].waypoints[agents[i].mission] - agents[i].pos).norm() >= 0.1)
+                        continue;
+                }
+                
+                /** @brief Publisher that publishes goal vector */
+                _goal_pub = _nh.advertise<geometry_msgs::PoseStamped>("/" + _id + "/goal", 20, true);
+                ros::Time start_time = ros::Time::now();
+                bool early_break = false;
+                
+                if (agent_waypoints[agents[i].id].waypoints.empty())
+                    continue;
+
+                if (agents[i].mission < (int)agent_waypoints[agents[i].id].waypoints.size()-1)
+                {
+                    agents[i].mission = agents[i].mission + 1;
+                }
+                else
+                    return;
+                
+                geometry_msgs::PoseStamped goal;
+                goal.pose.position.x = agent_waypoints[agents[i].id].waypoints[agents[i].mission].x(); 
+                goal.pose.position.y = agent_waypoints[agents[i].id].waypoints[agents[i].mission].y(); 
+                goal.pose.position.z = agent_waypoints[agents[i].id].waypoints[agents[i].mission].z();
+                
+
+                while (_goal_pub.getNumSubscribers() < 1) {
+                    // wait for a connection to publisher
+                    // you can do whatever you like here or simply do nothing
+                    if ((ros::Time::now() - start_time).toSec() > 2.0)
+                    {
+                        early_break = true;
+                        break;
+                    }
+                }
+
+                if (early_break)
+                    continue;
+
+                _goal_pub.publish(goal);
+                
+                start_time = ros::Time::now();
+                while ((ros::Time::now() - start_time).toSec() < 2.0)
+                {
+                    // Wait
+                }
+                
+                std::cout << "[user_server] " << KGRN << 
+                    "published waypoint " << agents[i].mission << KNRM << std::endl;
+                
+            }
         }
 
         void cloud_update_timer(const ros::TimerEvent &)
         {
+            if (!_valid_cloud)
+                return;
+            
+            cloud_msg.header.frame_id = "/world";
             _pcl_pub.publish(cloud_msg);
+
         }
         
         void pose_callback(const sensor_msgs::JointState::ConstPtr &msg)
@@ -123,6 +209,7 @@ class user_server_ros
                     new_agent.pos = nwu_transform.translation();
                     new_agent.q = nwu_transform.linear();
                     new_agent.t = msg->header.stamp;
+                    new_agent.mission = -1;
 
                     agents.push_back(new_agent);
                     return;
@@ -145,6 +232,7 @@ class user_server_ros
                 new_agent.pos = nwu_transform.translation();
                 new_agent.q = nwu_transform.linear();
                 new_agent.t = msg->header.stamp;
+                new_agent.mission = -1;
                 
                 agents.push_back(new_agent);
             }
@@ -155,9 +243,55 @@ class user_server_ros
 
         user_server_ros(ros::NodeHandle &nodeHandle) : _nh(nodeHandle)
         {
-            _nh.param<std::string>("file_location", _file_location, "/cloud.pcd");
+            _nh.param<std::string>("file_location", _file_location, "cloud.pcd");
             _nh.param<double>("tracker_timer_hz", _tracker_timer_hz, 5.0);
             _nh.param<double>("cloud_timer_hz", _cloud_hz, 2.0);
+            _nh.param<int>("agents", _agent_number, 1);
+            _nh.param<std::string>("single_or_multi", _single_or_multi, "single");
+            _nh.param<std::string>("formation", _formation, "antipodal");
+            std::vector<double> waypoint_vector;
+            _nh.getParam("waypoints", waypoint_vector);
+
+            if (_single_or_multi.compare("single") == 0)
+            {
+                _agent_number = 1;
+                agent_waypoint aw;
+                aw.id = 0;
+                // number of waypoints 
+                int size_of_waypoints = (int)waypoint_vector.size() / 3;
+                
+                for (int i = 0; i < size_of_waypoints; i++)
+                {
+                    aw.waypoints.push_back(
+                        Eigen::Vector3d(waypoint_vector[3*i + 0], 
+                        waypoint_vector[3*i + 1], 
+                        waypoint_vector[3*i + 2]));
+                }
+
+                std::cout << "[user_server] size of waypoints: " << KGRN 
+                    << aw.waypoints.size() << KNRM << std::endl;
+
+                agent_waypoints.push_back(aw);
+            }
+            else if (_single_or_multi.compare("multi") == 0)
+            {
+                std::cout << "[user_server] " << KRED
+                    << "multi agent mode detected" << KNRM << std::endl;
+                for (int i = 0; i < _agent_number; i++)
+                {
+                    agent_waypoint aw;
+                    aw.id = i;
+                    agent_waypoints.push_back(aw);
+                }
+            }
+            else
+            {
+                std::cout << "[user_server] " << KRED
+                    << "_single_or_multi not valid" << KNRM << std::endl;
+                return;
+            }
+
+            
 
             /** @brief Subscriber that receives position data of uavs published by simple_quad_simulator */
             _pose_sub = _nh.subscribe<sensor_msgs::JointState>(
@@ -176,19 +310,27 @@ class user_server_ros
 
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>); // Initialize the point cloud
 
+            std::cout << "[user_server] Opening pcd file from " << KGRN << _file_location << KNRM << " ..." << std::endl;
             ifstream myFile(_file_location);
             if(!myFile.fail()){
+                _valid_cloud = true;
                 pcl::io::loadPCDFile<pcl::PointXYZ>(_file_location, *cloud);// Load the pcd file
 
-                sensor_msgs::PointCloud2 cloud_msg;
                 pcl::toROSMsg(*cloud, cloud_msg);
+                std::cout << "[user_server] " << KGRN << 
+                    "pcd file found" << KNRM << " with size " << cloud->points.size() << std::endl;
             }    
             else
+            {
+                _valid_cloud = false;
                 std::cout << "[user_server] " << KRED << 
                     "No pcd file found" << KNRM << std::endl;
+            }
 
+            module_start_time = ros::Time::now();
             _target_tracker_timer.start();
             _cloud_timer.start();
+
         }
 
         ~user_server_ros(){}
